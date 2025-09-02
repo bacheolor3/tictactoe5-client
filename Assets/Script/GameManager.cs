c1ab28a2b912fdeb49ade839f359c4b978bcc5ce
bacheolor3 <bacheolor3@gmail.com>
2025-09-02 20:50:14 +0900

    On a warry

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    public void ChangeToGameScene()
+    {
+
+    }
+
+    public void ChangeToMainScene()
+    {
+
+    }
+
+    protected override void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        // TODO : Script that function for Scene Change
+    }    
+}
---
+++ Assets/Script/MainPanelController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MainPanelController : MonoBehaviour
+{
+    public void OnClickSinglePlayButton()
+    {
+
+    }
+
+    public void OnClickDualPlayButton()
+    {
+
+    }
+
+    public void OnClickMultiplePlayButton()
+    {
+
+    }
+
+    public void OnClickSettingsButton()
+    {
+
+    }
+}
